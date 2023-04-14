5159a3325f2f6bfe93a5b4baddcd220d8dc7f813
Walker Logan <walkerlogan@Walkers-Mini.lan>
2023-04-13 23:18:03 -0500

    final push

---
+++ README.md
@@ -0,0 +1,4 @@
+This is a planner for the work day.
+
+I used a whole lot of google and stack overflow to help me with the logic behind this project. It allowed me to deal with time, and dayjs as well as Jquery.
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,33 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+var timeDisplayEl = $("#currentDay");
+function displayTime() {
+  var rightNow = dayjs().format(
+    "[Current Date & Time:] MMM DD, YYYY [ at ] hh:mm:ss a"
+  );
+  timeDisplayEl.text(rightNow);
+}
+displayTime();
+setInterval(displayTime, 1000);
+
+$(".saveBtn").click(function () {
+  console.log(this);
+  var description = $(this).siblings(".description").val();
+  var rowTime = $(this).parent().attr("id");
+
+  localStorage.setItem(rowTime, description);
 });
+
+$(".time-block").each(function () {
+  var currentTime = dayjs().hour();
+  var rowHour = parseInt($(this).attr("id").split("-")[1]);
+  if (rowHour === currentTime) {
+    $(this).addClass("present");
+  } else if (rowHour > currentTime) {
+    $(this).addClass("future");
+  } else {
+    $(this).addClass("past");
+  }
+});
+
+for (i = 9; i < 18; i++) {
+  $(`#hour-${i} .description`).val(localStorage.getItem(`hour-${i}`));
+}
